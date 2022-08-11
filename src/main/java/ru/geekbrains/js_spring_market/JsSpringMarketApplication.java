2712292d53d76078f30f996ffafa57bf0ed6fcdd
sujane <sujane_mf@mail.ru>
2022-08-11 16:16:38 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/js_spring_market/JsSpringMarketApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.js_spring_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsSpringMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsSpringMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/geekbrains/js_spring_market/JsSpringMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.js_spring_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsSpringMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
